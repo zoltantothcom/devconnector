6829b5b71a25375228f24152b29d24e80b53d689
Zoltan Toth <contact@zoltantoth.com>
2018-07-07 07:22:02 -0400

    Creating the JWT

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://admin:admin123@ds125851.mlab.com:25851/vasarosnameny'
+  mongoURI: 'mongodb://admin:admin123@ds125851.mlab.com:25851/vasarosnameny',
+  secretOrKey: 'secret'
 }
