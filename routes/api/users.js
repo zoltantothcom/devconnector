f2fc4576b76adc4c3c3fbcae578400fcde31fe94
Zoltan Toth <contact@zoltantoth.com>
2018-07-12 05:26:40 -0400

    Passport JWT Authentication Strategy

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,7 @@ const gravatar = require('gravatar')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const keys = require('../../config/keys')
+const passport = require('passport')
 
 // Load user model
 const User = require('../../models/User')
@@ -105,6 +106,19 @@ router.post('/login', (req, res) => {
           })
         }
       })
+  })
+})
+
+// @route  GET api/users/current
+// @desc   Return current user
+// @access Private
+router.get('/current', passport.authenticate('jwt', {
+  session: false
+}), (req, res) => {
+  res.json({
+    id: req.user.id,
+    name: req.user.name,
+    email: req.user.email
   })
 })
 
