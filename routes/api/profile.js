307f4b3c6cbd58d0ed5f7dbe1ab296cb856c1403
Zoltan Toth <contact@zoltantoth.com>
2018-07-24 05:40:32 -0400

    Added route to get all profiles

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -40,6 +40,26 @@ router.get('/', passport.authenticate('jwt', {
     }).catch(err => res.status(404).json(err))
 })
 
+// @route  GET api/profile/all
+// @desc   Get all profiles
+// @access Public
+router.get('/all', (req, res) => {
+  const errors = {}
+
+  Profile.find()
+    .populate('user', ['name', 'avatar'])
+    .then(profiles => {
+      if (!profiles) {
+        errors.noprofiles = 'There are no profiles yet.'
+        res.status(404).json(errors)
+      }
+      res.json(profiles)
+    })
+    .catch(err => res.status(404).json({
+      profiles: 'There are no profiles yet.'
+    }))
+})
+
 // @route  GET api/profile/handle/:handle
 // @desc   Get profile by handle
 // @access Public
