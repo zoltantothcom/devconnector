470645bdf2e9908e19da6a812ad8744f7db0d436
Zoltan Toth <contact@zoltantoth.com>
2018-07-24 13:48:46 -0400

    Added user/profile remove route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -306,4 +306,26 @@ router.delete(
       .catch(err => res.status(404).json(err))
   })
 
+// @route  DELETE api/profile
+// @desc   Delete users profile
+// @access Private
+router.delete(
+  '/',
+  passport.authenticate('jwt', {
+    session: false
+  }),
+  (req, res) => {
+    Profile.findOneAndRemove({
+        user: req.user.id
+      })
+      .then(() => {
+        User.findOneAndRemove({
+            _id: req.user.id
+          })
+          .then(() => res.json({
+            success: true
+          }))
+      })
+  })
+
 module.exports = router
