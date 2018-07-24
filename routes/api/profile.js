b00cfa572427b883e4120437528e2ae492ec6c3c
Zoltan Toth <contact@zoltantoth.com>
2018-07-24 09:13:18 -0400

    Added experience route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -5,6 +5,7 @@ const passport = require('passport')
 
 // Load Validation
 const validateProfileInput = require('../../validation/profile')
+const validateExperienceInput = require('../../validation/experience')
 
 // Load Profile Model
 const Profile = require('../../models/Profile')
@@ -171,5 +172,44 @@ router.post('/', passport.authenticate('jwt', {
     }
   })
 })
+
+// @route  POST api/profile/experience
+// @desc   Add experience to profile
+// @access Private
+router.post(
+  '/experience',
+  passport.authenticate('jwt', {
+    session: false
+  }),
+  (req, res) => {
+    const {
+      errors,
+      isValid
+    } = validateExperienceInput(req.body)
+
+    // Check validation
+    if (!isValid) {
+      return res.status(400).json(errors)
+    }
+
+    Profile.findOne({
+      user: req.user.id
+    }).then(profile => {
+      const newExp = {
+        title: req.body.title,
+        company: req.body.company,
+        location: req.body.location,
+        from: req.body.from,
+        to: req.body.to,
+        current: req.body.current,
+        description: req.body.description
+      }
+
+      // Add to experience array
+      profile.experience.unshift(newExp)
+
+      profile.save().then(profile => res.json(profile))
+    })
+  })
 
 module.exports = router
---
+++ validation/experience.js
@@ -0,0 +1,27 @@
+const Validator = require('validator')
+const isEmpty = require('./is-empty')
+
+module.exports = function validateExperienceInput(data) {
+  let errors = {}
+
+  const title = !isEmpty(data.title) ? data.title : ''
+  const company = !isEmpty(data.company) ? data.company : ''
+  const fromDate = !isEmpty(data.from) ? data.from : ''
+
+  if (Validator.isEmpty(title)) {
+    errors.title = 'Job title is required.'
+  }
+
+  if (Validator.isEmpty(company)) {
+    errors.company = 'Company is required.'
+  }
+
+  if (Validator.isEmpty(fromDate)) {
+    errors.from = 'From date is required.'
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  }
+}
