3643650073c5f6185d1d7e5734967d25fe46edd4
Zoltan Toth <contact@zoltantoth.com>
2018-07-28 06:55:03 -0400

    Added a route to get all posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -16,6 +16,17 @@ router.get('/test', (req, res) => res.json({
   msg: 'Posts works'
 }))
 
+// @route  GET api/posts
+// @desc   Get posts
+// @access Public
+router.get('/', (req, res) => (
+  Post.find().sort({
+    date: -1
+  })
+  .then(posts => res.json(posts))
+  .catch(err => res.status(404))
+))
+
 // @route  POST api/posts
 // @desc   Create post
 // @access Private
