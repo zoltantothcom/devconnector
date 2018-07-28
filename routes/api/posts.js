e01868526fcb3a18d1a6586804f52e975217fc83
Zoltan Toth <contact@zoltantoth.com>
2018-07-28 06:59:05 -0400

    Getting single post by id

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -20,11 +20,23 @@ router.get('/test', (req, res) => res.json({
 // @desc   Get posts
 // @access Public
 router.get('/', (req, res) => (
-  Post.find().sort({
-    date: -1
-  })
-  .then(posts => res.json(posts))
-  .catch(err => res.status(404))
+Post.find().sort({
+  date: -1
+})
+.then(posts => res.json(posts))
+.catch(err => res.status(404)).json({
+  nopostsfound: 'No posts found.'
+}))))
+
+// @route  GET api/posts/:id
+// @desc   Get post by id
+// @access Public
+router.get('/:id', (req, res) => (
+  Post.findById(req.params.id)
+  .then(post => res.json(post))
+  .catch(err => res.status(404).json({
+    nopostfound: 'No post found.'
+  }))
 ))
 
 // @route  POST api/posts
