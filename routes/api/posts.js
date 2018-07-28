6ca402dbc8626245d59214e9c70d5d1e5a63a916
Zoltan Toth <contact@zoltantoth.com>
2018-07-28 07:18:34 -0400

    Delete Post route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -3,8 +3,9 @@ const router = express.Router()
 const mongoose = require('mongoose')
 const passport = require('passport')
 
-// Post Model
+// Post and Profile Models
 const Post = require('../../models/Post')
+const Profile = require('../../models/Profile')
 
 // Validation
 const validatePostInput = require('../../validation/post')
@@ -24,9 +25,9 @@ router.get('/', (req, res) => (
     date: -1
   })
   .then(posts => res.json(posts))
-  .catch(err => res.status(404)).json({
-    nopostfound: 'No post found.'
-  })
+  .catch(err => res.status(404).json({
+    nopostsfound: 'No posts found.'
+  }))
 ))
 
 // @route  GET api/posts/:id
@@ -63,6 +64,41 @@ router.post('/', passport.authenticate('jwt', {
   })
 
   newPost.save().then(post => res.json(post))
+})
+
+// @route  DELETE api/posts/:id
+// @desc   Delete post
+// @access Private
+router.delete('/:id', passport.authenticate('jwt', {
+  session: false
+}), (req, res) => {
+  Profile.findOne({
+      user: req.user.id
+    })
+    .then(profile => {
+      Post
+        .findById(req.params.id)
+        .then(post => {
+          if (post.user.toString() !== req.user.id) {
+            return res.status(401).json({
+              notauthorized: 'User not authorized.'
+            })
+          }
+
+          // Delete
+          post
+            .remove()
+            .then(() => res.json({
+              success: true
+            }))
+            .catch(err => res.status(404).json({
+              postnotfound: 'Post not found.'
+            }))
+        })
+        .catch(err => res.status(404).json({
+          postnotfound: 'Post not found.'
+        }))
+    })
 })
 
 module.exports = router
