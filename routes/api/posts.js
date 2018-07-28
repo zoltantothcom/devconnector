606b4fb25746328dd77871a0223ad750b6e2b6eb
Zoltan Toth <contact@zoltantoth.com>
2018-07-28 07:00:12 -0400

    Extra closing bracket removed

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -20,13 +20,14 @@ router.get('/test', (req, res) => res.json({
 // @desc   Get posts
 // @access Public
 router.get('/', (req, res) => (
-Post.find().sort({
-  date: -1
-})
-.then(posts => res.json(posts))
-.catch(err => res.status(404)).json({
-  nopostsfound: 'No posts found.'
-}))))
+  Post.find().sort({
+    date: -1
+  })
+  .then(posts => res.json(posts))
+  .catch(err => res.status(404)).json({
+    nopostfound: 'No post found.'
+  })
+))
 
 // @route  GET api/posts/:id
 // @desc   Get post by id
