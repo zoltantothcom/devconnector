d9bb27e35c37b27a2b5d409e25d30c2b50c2aecb
Zoltan Toth <contact@zoltantoth.com>
2018-08-12 14:58:11 -0400

    Education display and delete in Dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -6,6 +6,7 @@ import { getCurrentProfile, deleteAccount } from '../../actions/profileActions';
 import Spinner from '../common/Spinner';
 import ProfileActions from './ProfileActions';
 import Experience from './Experience';
+import Education from './Education';
 
 class Dashboard extends Component {
   componentDidMount() {
@@ -35,6 +36,8 @@ class Dashboard extends Component {
             <ProfileActions />
 
             <Experience data={profile.experience} />
+
+            <Education data={profile.education} />
 
             <div style={{ marginBottom: 60 }}>
               <button className="btn btn-danger" onClick={this.onDeleteClick}>
