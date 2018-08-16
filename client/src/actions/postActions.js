9e194b2f70f4e2858ec5dd02c7df08b6b3ee19a7
Zoltan Toth <contact@zoltantoth.com>
2018-08-16 16:03:00 -0400

    Displaying and deleting comments

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -6,9 +6,11 @@ import {
   POST_LOADING,
   GET_POST,
   DELETE_POST,
+  CLEAR_ERRORS,
 } from './types';
 
 export const addPost = postData => dispatch => {
+  dispatch(clearErrors());
   axios
     .post('/api/posts', postData)
     .then(res =>
@@ -105,8 +107,26 @@ export const removeLike = id => dispatch => {
 };
 
 export const addComment = (postId, commentData) => dispatch => {
+  dispatch(clearErrors());
   axios
     .post(`/api/posts/comment/${postId}`, commentData)
+    .then(res =>
+      dispatch({
+        type: GET_POST,
+        payload: res.data,
+      }),
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data,
+      }),
+    );
+};
+
+export const deleteComment = (postId, commentId) => dispatch => {
+  axios
+    .delete(`/api/posts/comment/${postId}/${commentId}`)
     .then(res =>
       dispatch({
         type: GET_POST,
@@ -124,5 +144,11 @@ export const addComment = (postId, commentData) => dispatch => {
 export const setPostLoading = () => {
   return {
     type: POST_LOADING,
+  };
+};
+
+export const clearErrors = () => {
+  return {
+    type: CLEAR_ERRORS,
   };
 };
